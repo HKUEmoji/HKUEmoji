ad708bd56fe1902ecff70c61dd07b288f3cea81a
WarnMark <wangyouan0629@hotmail.com>
2015-11-23 19:49:34 +0800

    Initial Commit

---
+++ HKUEmoji/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HKUEmoji
+//
+//  Created by Wang Youan on 23/11/2015.
+//  Copyright © 2015 Wang Youan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
