04c2f99d77ae15e877fe04c397654507ccbb70f0
WarnMark <wangyouan0629@hotmail.com>
2015-11-28 21:55:59 +0800

    finish navigation control

---
+++ HKUEmoji/StaticCamera.swift
@@ -0,0 +1,37 @@
+//
+//  StaticCamera.swift
+//  HKUEmoji
+//
+//  Created by Wang Youan on 28/11/2015.
+//  Copyright © 2015 Wang Youan. All rights reserved.
+//
+
+import UIKit
+
+class StaticCamera: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- HKUEmoji/ViewController.swift
+++ HKUEmoji/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
