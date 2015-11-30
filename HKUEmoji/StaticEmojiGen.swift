46b1b42176608a3835ea926f83549780616d6530
WarnMark <wangyouan0629@hotmail.com>
2015-11-30 10:47:45 +0800

    Add GPUImage

---
+++ HKUEmoji/StaticEmojiGen.swift
@@ -0,0 +1,44 @@
+//
+//  StaticEmojiGen.swift
+//  HKUEmoji
+//
+//  Created by Wang Youan on 30/11/2015.
+//  Copyright © 2015 Wang Youan. All rights reserved.
+//
+
+import UIKit
+import GPUImage
+
+class StaticEmojiGen: UIViewController {
+
+    @IBOutlet weak var imageShow: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        let image: UIImage = UIImage(named: "test.png")!
+        imageShow.image = image
+
+//        imageShow = init()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
