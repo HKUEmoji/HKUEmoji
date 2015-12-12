0c2e27140acba327777017dc541f1cfc4c6ff703
WarnMark <wangyouan0629@hotmail.com>
2015-12-12 18:10:37 +0800

    add new image

--- HKUEmoji/Dynamic.swift
+++ HKUEmoji/Dynamic.swift
@@ -8,174 +8,142 @@
 
 import UIKit
 
-class Dynamic: UIViewController{
-    
-    
-    @IBOutlet weak var gifImage: UIImageView!
-    
-    @IBOutlet weak var gifTemplate1: UIImageView!
-    
-    @IBOutlet weak var gifTemplate2: UIImageView!
-    @IBOutlet weak var gifTemplate3: UIImageView!
-    
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        gifTemplate1.userInteractionEnabled = true
-        gifTemplate2.userInteractionEnabled = true
-        gifTemplate3.userInteractionEnabled = true
-        
-        
-        
-        let tapGR1 = UITapGestureRecognizer(target: self, action:"tapHandler1:")
-        gifTemplate1.addGestureRecognizer(tapGR1)
-        
-        
-        gifTemplate1.animationImages = NSArray() as! [UIImage];
-        gifTemplate1.animationImages?.append(UIImage(named: "hou2.png")!);
-        gifTemplate1.animationImages?.append(UIImage(named: "hou5.png")!);
- 
-        gifTemplate1.animationDuration = 1
-        gifTemplate1.animationRepeatCount = 0
-        gifTemplate1.startAnimating()
-        
-        
-        let tapGR2 = UITapGestureRecognizer(target: self, action:"tapHandler2:")
-        gifTemplate2.addGestureRecognizer(tapGR2)
-        
-        
-        gifTemplate2.animationImages = NSArray() as! [UIImage];
-        gifTemplate2.animationImages?.append(UIImage(named: "hou1.png")!);
-        gifTemplate2.animationImages?.append(UIImage(named: "hou3.png")!);
-        
-        gifTemplate2.animationDuration = 1
-        gifTemplate2.animationRepeatCount = 0
-        gifTemplate2.startAnimating()
-        
-        let tapGR3 = UITapGestureRecognizer(target: self, action:"tapHandler3:")
-        gifTemplate3.addGestureRecognizer(tapGR3)
-        
-        
-        gifTemplate3.animationImages = NSArray() as! [UIImage];
-        gifTemplate3.animationImages?.append(UIImage(named: "hou4.png")!);
-        gifTemplate3.animationImages?.append(UIImage(named: "hou6.png")!);
-        
-        gifTemplate3.animationDuration = 1
-        gifTemplate3.animationRepeatCount = 0
-        gifTemplate3.startAnimating()
-        
-        
-  /*      gifImage.animationImages = NSArray() as! [UIImage];
-        gifImage.animationImages?.append(UIImage(named: "dynamic1.jpg")!);
-        gifImage.animationImages?.append(UIImage(named: "dynamic2.jpg")!);
-        
+class Dynamic: UIViewController {
 
-        
-        gifImage.animationDuration = 1
-        gifImage.animationRepeatCount = 0
-        gifImage.startAnimating()
-*/
-   /*   gifTemplate1.image = UIImage(named: "dynamic1.jpg")
-        
-        gifTemplate2.animationImages = NSArray() as! [UIImage];
-        gifTemplate2.animationImages?.append(UIImage(named: "dynamic1.jpg")!);
-        gifTemplate2.animationImages?.append(UIImage(named: "dynamic2.jpg")!);
-        
-        
-        
-        gifTemplate2.animationDuration = 1
-        gifTemplate2.animationRepeatCount = 0
-        gifTemplate2.startAnimating()
- */
-        
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    func tapHandler1(sender:UITapGestureRecognizer){
-        gifImage.animationImages = NSArray() as! [UIImage];
-        gifImage.animationImages?.append(UIImage(named: "dynamic1.jpg")!);
-        gifImage.animationImages?.append(UIImage(named: "dynamic2.jpg")!);
-        
-        
-        
-        gifImage.animationDuration = 1
-        gifImage.animationRepeatCount = 0
-        gifImage.startAnimating()
-        
-    }
-    
-    func tapHandler2(sender:UITapGestureRecognizer){
-        gifImage.animationImages = NSArray() as! [UIImage];
-        gifImage.animationImages?.append(UIImage(named: "hou1.png")!);
-        gifImage.animationImages?.append(UIImage(named: "hou2.png")!);
-        
-        
-        
-        gifImage.animationDuration = 1
-        gifImage.animationRepeatCount = 0
-        gifImage.startAnimating()
-        
-    }
-    
-  /*  func tapHandler3(sender:UITapGestureRecognizer){
-        gifImage.animationImages = NSArray() as! [UIImage];
-        
-        addFaceToBackGround(5, backgroundImage: <#T##UIImage#>, faceImage: <#T##UIImage#>)
-        gifImage.animationImages?.append(UIImage(named: "dynamic1.jpg")!);
-        gifImage.animationImages?.append(UIImage(named: "mouth.png")!);
-        
-        
-        
-        gifImage.animationDuration = 1
-        gifImage.animationRepeatCount = 0
-        gifImage.startAnimating()
-        
-    }
+
+	@IBOutlet weak var gifImage: UIImageView!
+
+	@IBOutlet weak var gifTemplate1: UIImageView!
+
+	@IBOutlet weak var gifTemplate2: UIImageView!
+	@IBOutlet weak var gifTemplate3: UIImageView!
+
+    var faceBound = CGRect(x: 196, y: 135, width: 230, height: 230)
+    var faceImage = UIImage(named: "test1.png")
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		gifTemplate1.userInteractionEnabled = true
+		gifTemplate2.userInteractionEnabled = true
+		gifTemplate3.userInteractionEnabled = true
+
+		let tapGR1 = UITapGestureRecognizer(target: self, action: "tapHandler1:")
+		gifTemplate1.addGestureRecognizer(tapGR1)
+
+		gifTemplate1.animationImages = NSArray() as? [UIImage] ;
+		gifTemplate1.animationImages?.append(UIImage(named: "hou2.png")!) ;
+		gifTemplate1.animationImages?.append(UIImage(named: "hou5.png")!) ;
+
+		gifTemplate1.animationDuration = 1
+		gifTemplate1.animationRepeatCount = 0
+		gifTemplate1.startAnimating()
+
+		let tapGR2 = UITapGestureRecognizer(target: self, action: "tapHandler2:")
+		gifTemplate2.addGestureRecognizer(tapGR2)
+
+		gifTemplate2.animationImages = NSArray() as? [UIImage] ;
+		gifTemplate2.animationImages?.append(UIImage(named: "hou1.png")!) ;
+		gifTemplate2.animationImages?.append(UIImage(named: "hou3.png")!) ;
+
+		gifTemplate2.animationDuration = 1
+		gifTemplate2.animationRepeatCount = 0
+		gifTemplate2.startAnimating()
+
+		let tapGR3 = UITapGestureRecognizer(target: self, action: "tapHandler3:")
+		gifTemplate3.addGestureRecognizer(tapGR3)
+
+
+		gifTemplate3.animationImages = NSArray() as? [UIImage] ;
+		gifTemplate3.animationImages?.append(UIImage(named: "hou4.png")!) ;
+		gifTemplate3.animationImages?.append(UIImage(named: "hou6.png")!) ;
+
+		gifTemplate3.animationDuration = 1
+		gifTemplate3.animationRepeatCount = 0
+		gifTemplate3.startAnimating()
+
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+	func tapHandler1(sender: UITapGestureRecognizer) {
+		gifImage.animationImages = NSArray() as? [UIImage] ;
+		gifImage.animationImages?.append(UIImage(named: "hou3.jpg")!) ;
+		gifImage.animationImages?.append(UIImage(named: "hou4.jpg")!) ;
+
+		gifImage.animationDuration = 1
+		gifImage.animationRepeatCount = 0
+		gifImage.startAnimating()
+
+	}
+
+	func tapHandler2(sender: UITapGestureRecognizer) {
+		gifImage.animationImages = NSArray() as? [UIImage] ;
+		gifImage.animationImages?.append(UIImage(named: "hou1.png")!) ;
+		gifImage.animationImages?.append(UIImage(named: "hou2.png")!) ;
+
+		gifImage.animationDuration = 1
+		gifImage.animationRepeatCount = 0
+		gifImage.startAnimating()
+
+	}
     
     func addFaceToBackGround(faceBound: CGRect, backgroundImage: UIImage, faceImage: UIImage) -> UIImage {
-        //        let ratio = min(faceBound.height / faceImage.size.height, faceBound.width / faceImage.size.width)
-        //        let newFaceImage = StaticEmojiGen.resizeImageFromRatio(faceImage, ratio: Double(ratio))
         UIGraphicsBeginImageContextWithOptions(backgroundImage.size, false, 0.0)
-        backgroundImage.drawInRect(CGRect(origin: CGPointZero, size: backgroundImage.size))
         faceImage.drawInRect(faceBound)
+        backgroundImage.drawInRect(CGRect(origin: CGPointZero, size: backgroundImage.size))
         
         let newImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         return newImage
     }
     
-    
-    func faceBoundDetection(originPic: UIImage) -> CGRect {
-        let inputImage = CIImage(image: originPic)
-        let detector = CIDetector(ofType: CIDetectorTypeFace, context: context,
-            options: [CIDetectorAccuracy: CIDetectorAccuracyHigh])
-        var faceFeatures: [CIFaceFeature]!
-        if let orientation: AnyObject = inputImage?.properties[kCGImagePropertyOrientation as String] {
-            faceFeatures = detector.featuresInImage(inputImage!,
-                options: [CIDetectorImageOrientation: orientation]
-                ) as! [CIFaceFeature]
-        } else {
-            faceFeatures = detector.featuresInImage(inputImage!) as! [CIFaceFeature]
-        }
+    func tapHandler3(sender:UITapGestureRecognizer){
+        gifImage.animationImages = NSArray() as? [UIImage];
         
-        var transform = CGAffineTransformMakeScale(1 / originPic.scale, 1 / originPic.scale)
-        var bounds = CGRectApplyAffineTransform(faceFeatures[self.currentImage.1].bounds, transform)
-        
-        transform = CGAffineTransformMakeScale(1, -1)
-        transform = CGAffineTransformTranslate(transform, 0, -originPic.size.height)
-        bounds = CGRectApplyAffineTransform(bounds, transform)
-        bounds.size.height *= 1.25
-        bounds.size.width *= 1.1
-        return bounds
-    }
+        let image1 = addFaceToBackGround(faceBound, backgroundImage: UIImage(named: "hou1.png")!, faceImage: faceImage!)
+        let image2 = addFaceToBackGround(faceBound, backgroundImage: UIImage(named: "hou6.png")!, faceImage: faceImage!)
+        gifImage.animationImages?.append(image1);
+        gifImage.animationImages?.append(image2);
 
-    */
+        gifImage.animationDuration = 1
+        gifImage.animationRepeatCount = 0
+        gifImage.startAnimating()
+        
+    }
+	/*
+
+
+
+
+	 func faceBoundDetection(originPic: UIImage) -> CGRect {
+	 let inputImage = CIImage(image: originPic)
+	 let detector = CIDetector(ofType: CIDetectorTypeFace, context: context,
+	 options: [CIDetectorAccuracy: CIDetectorAccuracyHigh])
+	 var faceFeatures: [CIFaceFeature]!
+	 if let orientation: AnyObject = inputImage?.properties[kCGImagePropertyOrientation as String] {
+	 faceFeatures = detector.featuresInImage(inputImage!,
+	 options: [CIDetectorImageOrientation: orientation]
+	 ) as! [CIFaceFeature]
+	 } else {
+	 faceFeatures = detector.featuresInImage(inputImage!) as! [CIFaceFeature]
+	 }
+
+	 var transform = CGAffineTransformMakeScale(1 / originPic.scale, 1 / originPic.scale)
+	 var bounds = CGRectApplyAffineTransform(faceFeatures[self.currentImage.1].bounds, transform)
+
+	 transform = CGAffineTransformMakeScale(1, -1)
+	 transform = CGAffineTransformTranslate(transform, 0, -originPic.size.height)
+	 bounds = CGRectApplyAffineTransform(bounds, transform)
+	 bounds.size.height *= 1.25
+	 bounds.size.width *= 1.1
+	 return bounds
+	 }
+
+	 */
 }
 
 
